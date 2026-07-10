a5236f7064fedb4c87376418da501da590adf73e
itsAdil45 <sheikhadil086@gmail.com>
2026-07-10 16:23:07 +0500

    add service worker

--- src/app/(app)/layout.jsx
+++ src/app/(app)/layout.jsx
@@ -1,6 +1,7 @@
 "use client";
 import BottomTabBar from "@/components/Menus/BottomTabBar";
 import BackToHomeGuard from "@/components/BackToHomeGuard";
+import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 import { useStatusBar } from "@/hook/useStatusBar";
 export default function AppLayout({ children }) {
   useStatusBar();
@@ -9,6 +10,7 @@ export default function AppLayout({ children }) {
     <div className="relative min-h-screen bg-zinc-50">
       <BackToHomeGuard />
       <div className="pb-20">{children}</div>
+      <ServiceWorkerRegister />
       <BottomTabBar />
     </div>
   );
