1567763790e85d3711cc2bb8aa8605d87afb5afd
itsAdil45 <sheikhadil086@gmail.com>
2026-07-13 19:52:16 +0500

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import OfflineProvider from "@/components/OfflineProvider";
+import OfflineProvider from "@/components/OfflineProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +25,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         {" "}
-        {children}
-        {/* <OfflineProvider></OfflineProvider> */}
+        <OfflineProvider>{children}</OfflineProvider>
       </body>
     </html>
   );
