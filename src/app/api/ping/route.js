d884162c28063efd4e62e30527a4620797873a88
itsAdil45 <sheikhadil086@gmail.com>
2026-07-13 19:38:08 +0500

    add ping

---
+++ src/app/api/ping/route.js
@@ -0,0 +1,7 @@
+// app/api/ping/route.js
+export async function GET() {
+  return new Response("ok", {
+    status: 200,
+    headers: { "Cache-Control": "no-store" },
+  });
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import OfflineProvider from "@/components/OfflineProvider";
-import BackToHomeGuard from "@/components/BackToHomeGuard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +25,6 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         {" "}
-        <BackToHomeGuard />
         <OfflineProvider>{children}</OfflineProvider>
       </body>
     </html>
